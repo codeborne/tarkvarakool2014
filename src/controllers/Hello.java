d31ea640e50ae8836161d1dd918eb59302e9ed1b
Anton Keks <anton@azib.net>
2014-10-10 00:43:17 +0300

    getNames() is not used anymore

--- src/controllers/Hello.java
+++ src/controllers/Hello.java
@@ -2,14 +2,6 @@ package controllers;
 
 import framework.Controller;
 
-import java.util.List;
-
-import static java.util.Arrays.asList;
-
 public class Hello extends Controller {
   public String name;
-
-  public List<String> getNames() {
-    return asList("Foo", "Bar", "Baz");
-  }
 }
