d482e25c695535a193ae880e78679f5db8f42f57
Anton Keks <anton@azib.net>
2014-10-09 23:17:08 +0300

    fix handler tests after behaviour changes

--- test/framework/HandlerTest.java
+++ test/framework/HandlerTest.java
@@ -56,29 +56,15 @@ public class HandlerTest {
 
   @Test
   public void bindRequestWithoutRequestField() throws Exception {
-    handler.bindFrameworkFields(new Object(), request);
+    handler.bindFrameworkFields(new Object(), request, response);
   }
 
   @Test
-  public void bindRequestWithRequestField() throws Exception {
+  public void bindRequestToController() throws Exception {
     HttpServletRequest request = mock(HttpServletRequest.class);
-    class Foo {
-      public HttpServletRequest request;
-    }
-    Foo foo = new Foo();
-    handler.bindFrameworkFields(foo, request);
+    Controller foo = new Controller() {};
+    handler.bindFrameworkFields(foo, request, response);
     assertThat(foo.request, sameInstance(request));
-  }
-
-  @Test
-  public void bindRequestWithRequestFieldWithDifferentName() throws Exception {
-    HttpServletRequest request = mock(HttpServletRequest.class);
-    class Foo {
-      public HttpServletRequest httpRequest;
-    }
-    Foo foo = new Foo();
-    handler.bindFrameworkFields(foo, request);
-    assertThat(foo.httpRequest, sameInstance(request));
   }
 
   @Test
@@ -136,7 +122,7 @@ public class HandlerTest {
   public void handleException() throws Exception {
     handler.handleException(new InvocationTargetException(new RuntimeException()), response);
 
-    verify(response).sendError(SC_INTERNAL_SERVER_ERROR);
+    verify(response).sendError(eq(SC_INTERNAL_SERVER_ERROR), anyString());
     verifyNoMoreInteractions(response);
   }
 
