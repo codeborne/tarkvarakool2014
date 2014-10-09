2fb4a51f667875d1bd265eaa372ba06141dca5bd
Kätlin Hein, Ülla Poola <ktlin@codeborne.com, lla@codeborne.com>
2014-10-09 12:01:46 +0300

    add first trial

--- src/controllers/Home.java
+++ src/controllers/Home.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 public class Home extends Controller {
 
   public void get() {
+      System.out.println("Get was called");
   }
 
   public int getParameterCount() {
