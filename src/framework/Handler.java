2fabe543fe4add746e4f91bd051cbd6b45b51939
Anton Keks <anton@azib.net>
2014-10-10 00:23:48 +0300

    fix import

--- src/framework/Handler.java
+++ src/framework/Handler.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.hibernate.SessionFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
