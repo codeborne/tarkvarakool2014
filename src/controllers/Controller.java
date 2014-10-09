0ed4f82a2fbe6e59ddeb64119ffd309b0dfae61f
Anton Keks <anton@azib.net>
2014-10-09 22:45:36 +0300

    give a hint to extending classes that these http methods can be overridden

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -4,4 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 
 public abstract class Controller {
   public HttpServletRequest request;
+
+  public void get() {
+  }
+
+  public void post() {
+  }
+
+  public void put() {
+  }
+
+  public void delete() {
+  }
+
+  public void options() {
+  }
 }
