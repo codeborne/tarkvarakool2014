3d098a9d2b8f1b9a6db29500d8a8c59f5813d2fd
Anton Keks <anton@azib.net>
2014-10-10 12:58:01 +0300

    redirect now supports a parameter

--- src/framework/Redirect.java
+++ src/framework/Redirect.java
@@ -1,7 +1,27 @@
 package framework;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class Redirect extends RuntimeException {
   public Redirect(String path) {
     super(path);
+  }
+
+  public Redirect(String path, String paramName, String paramValue) {
+    this(path + "?" + paramName + "=" + urlEncode(paramValue));
+  }
+
+  public String getPath() {
+    return getMessage();
+  }
+
+  private static String urlEncode(String paramValue) {
+    try {
+      return URLEncoder.encode(paramValue, Handler.THE_ENCODING);
+    }
+    catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
