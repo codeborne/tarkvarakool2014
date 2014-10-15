04efc9bc0da9a2c7affbb34e740a5c4339f8d7f3
Monika Põldma, Ülla Poola <monika@codeborne.com, lla@codeborne.com>
2014-10-15 15:45:37 +0300

    Modify tests refactored

--- test/controllers/ModifyTest.java
+++ test/controllers/ModifyTest.java
@@ -2,166 +2,148 @@ package controllers;
 
 import framework.Redirect;
 import model.Goal;
-import org.hibernate.Session;
 import org.hibernate.exception.ConstraintViolationException;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-public class ModifyTest {
+public class ModifyTest extends ControllerTest<Modify>{
+
 
   @Test
   public void testGetWhenObjectFound() {
-    Session hibernate = mock(Session.class);
+    controller.id = 2L;
 
-    Modify modify = new Modify();
-    modify.id = 2L;
-    modify.hibernate = hibernate;
+    when(hibernate.get(Goal.class, 2L)).thenReturn(new Goal("name", 10));
 
-    Goal expectedGoal = new Goal("name", 300);
-    when(hibernate.get(Goal.class, 2L)).thenReturn(expectedGoal);
+    controller.get();
 
-    modify.get();
+    assertEquals("name", controller.name);
+    assertEquals(10, (int) controller.budget);
 
-    assertEquals("name", modify.name);
-    assertEquals(300, (int)modify.budget);
   }
+
 
   @Test
   public void testGetWhenObjectNotFound() {
-    Session hibernate = mock(Session.class);
-
-    Modify modify = new Modify();
-    modify.id = 2L;
-    modify.hibernate = hibernate;
-
+    controller.id = 2L;
     when(hibernate.get(Goal.class, 2L)).thenReturn(null);
 
     try {
-      modify.get();
+      controller.get();
       fail();
     } catch (Redirect e) {
       assertEquals("add", e.getMessage());
+
     }
   }
+
 
   @Test
   public void postTestWithNoErrors() {
-    Session hibernate = mock(Session.class);
-    Modify modify = new Modify();
-    modify.id = 2L;
-    modify.hibernate = hibernate;
-    modify.name = "nimi";
-    modify.budget = 600;
-    Goal expectedGoal = new Goal(modify.name, modify.budget);
+    controller.id = 2L;
+    controller.name = "name";
+    controller.budget = 10;
 
-    Goal goalBeingChanged = new Goal("nimi", 500);
+
+    Goal expectedGoal = new Goal(controller.name, controller.budget);
+    Goal goalBeingChanged = new Goal("name", 10);
     when(hibernate.get(Goal.class, 2L)).thenReturn(goalBeingChanged);
 
+
     try {
-      modify.post();
+      controller.post();
       fail();
-    } catch (Redirect e) {
-      Assert.assertEquals("goals", e.getMessage());
-      Assert.assertEquals(expectedGoal.getName(), goalBeingChanged.getName());
-      Assert.assertEquals(expectedGoal.getBudget(), goalBeingChanged.getBudget());
+    } catch (Exception e) {
+      assertEquals("goals", e.getMessage());
+      assertEquals(expectedGoal.getBudget(), goalBeingChanged.getBudget());
+      assertEquals(expectedGoal.getName(), goalBeingChanged.getName());
 
     }
-
   }
-
   @Test
   public void postTestIfNameNull() {
-    Modify modify = new Modify();
-    modify.name = null;
-    modify.budget = 2;
+    controller.name = null;
+    controller.budget = 10;
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Sisestage eesmärk."));
+    controller.post();
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage eesmärk."));
+
   }
 
   @Test
   public void postTestIfNameIsEmpty() {
-    Modify modify = new Modify();
-    modify.name = "";
-    modify.budget = 2;
+    controller.name = "";
+    controller.budget = 2;
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Sisestage eesmärk."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage eesmärk."));
   }
 
   @Test
   public void postTestIfBudgetNull() {
-    Modify modify = new Modify();
-    modify.name = "name";
-    modify.budget = null;
+    controller.name = "name";
+    controller.budget = null;
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Sisestage korrektne eelarve."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage korrektne eelarve."));
   }
 
   @Test
   public void postTestIfBudgetIsNegative() {
-    Modify modify = new Modify();
-    modify.name = "name";
-    modify.budget = -1;
+    controller.name = "name";
+    controller.budget = -1;
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Sisestage korrektne eelarve."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage korrektne eelarve."));
   }
 
   @Test
-  public void postIfBudgetConversionFails(){
-    Modify modify = new Modify();
-    modify.name = "name";
-    modify.budget = 1;
-    modify.errors.put("budget", new NumberFormatException());
+  public void postIfBudgetConversionFails() {
+    controller.name = "name";
+    controller.budget = 1;
+    controller.errors.put("budget", new NumberFormatException());
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Sisestage korrektne eelarve."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage korrektne eelarve."));
   }
 
   @Test
   public void postIfErrorsContainsName() {
-    Modify modify = new Modify();
-    modify.name="name";
-    modify.budget = 1;
-    modify.errors.put("name", new RuntimeException());
+    controller.name = "name";
+    controller.budget = 1;
+    controller.errors.put("name", new RuntimeException());
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Tekkis viga."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
 
   }
 
   @Test
   public void postIfErrorsContainsBudget() {
-    Modify modify = new Modify();
-    modify.name="name";
-    modify.budget = 1;
-    modify.errors.put("budget", new Exception());
+    controller.name = "name";
+    controller.budget = 1;
+    controller.errors.put("budget", new Exception());
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Tekkis viga."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
 
   }
 
   @Test
   public void postWithDuplicateGoal() {
-    Modify modify = new Modify();
-    modify.name = "name";
-    modify.budget = 1;
-    modify.id = 2L;
-    Session hibernate = mock(Session.class);
-    modify.hibernate = hibernate;
+    controller.name = "name";
+    controller.budget = 1;
+    controller.id = 2L;
 
     Goal expectedGoal = new Goal("goal", 100);
 
@@ -170,19 +152,16 @@ public class ModifyTest {
     ConstraintViolationException expectedException = mock(ConstraintViolationException.class);
     doThrow(expectedException).when(hibernate).update(any(Goal.class));
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("See eesmärk on juba sisestatud."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("See eesmärk on juba sisestatud."));
   }
 
   @Test
   public void postUpdateFailsWithException() {
-    Modify modify = new Modify();
-    modify.name = "name";
-    modify.budget = 1;
-    modify.id = 2L;
-    Session hibernate = mock(Session.class);
-    modify.hibernate = hibernate;
+    controller.name = "name";
+    controller.budget = 1;
+    controller.id = 2L;
 
     Goal expectedGoal = new Goal("goal", 100);
 
@@ -191,9 +170,9 @@ public class ModifyTest {
     Exception expectedException = new RuntimeException();
     doThrow(expectedException).when(hibernate).update(any(Goal.class));
 
-    modify.post();
-    assertEquals(1, modify.errorsList.size());
-    assertTrue(modify.errorsList.contains("Tekkis viga."));
+    controller.post();
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
   }
 }
 
