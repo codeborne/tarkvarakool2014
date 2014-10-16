6a29e7c63843fadc63183c6574f658ff10e117f9
xp, Erik Jõgi <xp, erik@codeborne.com>
2014-10-16 08:16:31 +0300

    removes old experiments

--- src/controllers/Localize.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-
-import framework.Controller;
-
-public class Localize extends Controller {
-  public String hello = messages.get("hello");
-}
--- src/controllers/admin/Home.java
+++
@@ -1,8 +0,0 @@
-package controllers.admin;
-
-import framework.Controller;
-
-public class Home extends Controller {
-  public void get() {
-  }
-}
