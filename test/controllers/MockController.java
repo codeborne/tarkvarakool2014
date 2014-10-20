04f3b09ad547b2d301c4460592bd53384a61014f
Jarmo Pertman <jarmo.p@gmail.com>
2014-10-20 19:38:35 +0300

    Revert "Add experimental routes file support."
    This reverts commit 70a0421e33f0da1e91103f670871c6ee513712e8.

--- test/controllers/MockController.java
+++ test/controllers/MockController.java
@@ -1,11 +1,6 @@
 package controllers;
 
 import framework.Controller;
-import framework.Result;
 
 public class MockController extends Controller {
-  public Result action() throws Exception {
-    return render();
-  }
-  public Result secondAction() { return render(); }
 }
