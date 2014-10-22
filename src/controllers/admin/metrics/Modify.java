952849cb68288f52b9bb87d0ff9984b5221d25c0
Ülla Poola, Kätlin Hein <lla@codeborne.com, ktlin@codeborne.com>
2014-10-22 14:28:39 +0300

    added super class for metrics Add and Modify. added corresponding test classes

---
+++ src/controllers/admin/metrics/Modify.java
@@ -0,0 +1,9 @@
+package controllers.admin.metrics;
+
+public class Modify extends Save {
+
+  @Override
+  protected void save(){
+
+  }
+}
---
+++ test/controllers/admin/metrics/ModifyTest.java
@@ -0,0 +1,11 @@
+package controllers.admin.metrics;
+
+import controllers.ControllerTest;
+import org.junit.Test;
+
+public class ModifyTest extends ControllerTest<Modify> {
+  @Test
+  public void testName() throws Exception {
+
+  }
+}
