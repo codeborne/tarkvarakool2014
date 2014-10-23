2415916e55ba875378bccaa1e145d2579fb27e63
Monika Põldma, Ülla Poola <monika@codeborne.com, lla@codeborne.com>
2014-10-23 14:53:27 +0300

    Add fake values for testing purposes

--- src/controllers/Test.java
+++ src/controllers/Test.java
@@ -6,6 +6,9 @@ import framework.Role;
 import model.Goal;
 import model.Metric;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+
 //TODO remove the whole class when done with testing
 public class Test extends UserAwareController {
 
@@ -27,9 +30,27 @@ public class Test extends UserAwareController {
     hibernate.flush();
     hibernate.save(new Metric(goal2, "A metric", "", "", 8, "", 33, "", "cnf tgh", "aa"));
     hibernate.flush();
-    hibernate.save(new Metric(goal2, "ABC metric", "", "abc", 0, "", 0, "", "dfghdtgfy iutyuio", ""));
+
+    Metric metric = new Metric(goal2, "ABC metric", "", "abc", 0, "", 0, "", "dfghdtgfy iutyuio", "");
+    metric.setValues(new HashMap<Integer, BigDecimal>() {{
+      put(2014, new BigDecimal(55555));
+      put(2015, new BigDecimal(888888));
+      put(2016, new BigDecimal(12));
+      put(2017, new BigDecimal(222222));
+      put(2018, new BigDecimal(20000));
+    }});
+    hibernate.save(metric);
     hibernate.flush();
-    hibernate.save(new Metric(goal2, "DEF metric", "", "def", 0, "", 0, "", "dsfhgf iutyuio", "dsfhgjk"));
+
+    metric = new Metric(goal2, "DEF metric", "", "def", 0, "", 0, "", "dsfhgf iutyuio", "dsfhgjk");
+    metric.setValues(new HashMap<Integer, BigDecimal>() {{
+      put(2015, new BigDecimal(555));
+      put(2016, new BigDecimal(200));
+      put(2018, new BigDecimal(1233));
+      put(2019, new BigDecimal(9999));
+      put(2020, new BigDecimal(12));
+    }});
+    hibernate.save(metric);
     hibernate.flush();
 
     return redirect(Value.class);
