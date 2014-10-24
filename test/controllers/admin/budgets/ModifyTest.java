3aed56d22983b9080bb07f6bde9fa9e696d4392f
Ülla Poola, Kätlin Hein <lla@codeborne.com, ktlin@codeborne.com>
2014-10-24 12:06:44 +0300

    added yearly budgets modifying test

---
+++ test/controllers/admin/budgets/ModifyTest.java
@@ -0,0 +1,159 @@
+package controllers.admin.budgets;
+
+import controllers.ControllerTest;
+import model.Goal;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.SimpleExpression;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class ModifyTest extends ControllerTest<Modify> {
+  @Test
+  public void postIfErrorsContainsGoalId() throws Exception {
+    controller.yearlyBudget = 123L;
+    controller.errors.put("goalId", new RuntimeException());
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+  }
+
+  @Test
+  public void postIfGoalIdIsNull() throws Exception {
+    controller.yearlyBudget = 145L;
+    controller.year = 2019;
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+  }
+
+  @Test
+  public void postIfErrorsContainsYear() throws Exception {
+    controller.yearlyBudget = 123L;
+    controller.errors.put("year", new RuntimeException());
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+  }
+
+  @Test
+  public void postIfYearIsNull() throws Exception {
+    controller.yearlyBudget = 145L;
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+
+  }
+
+  @Test
+  public void postIfYearIsBiggerThanAllowed() throws Exception {
+    controller.goalId = 45L;
+    controller.year = 2060;
+    controller.yearlyBudget = 150L;
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+
+  }
+
+  @Test
+  public void postIfYearIsSmallerThanAllowed() throws Exception {
+    controller.goalId = 45L;
+    controller.year = 2010;
+    controller.yearlyBudget = 150L;
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+
+  }
+
+  @Test
+  public void postIfErrorsContainsYearlyBudget() throws Exception {
+    controller.goalId = 45L;
+    controller.year = 2017;
+    controller.errors.put("yearlyBudget", new RuntimeException());
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage korrektne väärtus."));
+  }
+
+  @Test
+  public void postIfYearlyBudgetIsNull() throws Exception {
+    controller.goalId = 45L;
+    controller.year = 2017;
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage korrektne väärtus."));
+  }
+
+
+  @Test
+  public void postIfYearlyBudgetIsNegative() throws Exception {
+    controller.goalId = 45L;
+    controller.year = 2017;
+    controller.yearlyBudget = -14L;
+
+    assertRender(controller.post());
+
+    assertEquals(1, controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Sisestage korrektne väärtus."));
+  }
+
+  @Test
+  public void postSuccess() throws Exception {
+    controller.goalId = 1L;
+    controller.year = 2015;
+    controller.yearlyBudget = 555L;
+    Criteria criteria = mock(Criteria.class);
+    when(hibernate.createCriteria(Goal.class)).thenReturn(criteria);
+    when(criteria.add(any(SimpleExpression.class))).thenReturn(criteria);
+    when(criteria.list()).thenReturn(Arrays.asList(new Goal("some goal", 1000)));
+
+    assertRender(controller.post());
+
+    Goal updatedGoal = (Goal) getUpdatedEntity();
+    assertTrue(controller.errorsList.isEmpty());
+    assertTrue(updatedGoal.getYearlyBudgets().containsKey(2015));
+    Assert.assertEquals((Long) 555L, updatedGoal.getYearlyBudgets().get(2015));
+    verify(hibernate).update(updatedGoal);
+
+  }
+
+  @Test
+  public void postFailure() throws Exception {
+    controller.goalId = 1L;
+    controller.year = 2015;
+    controller.yearlyBudget = 555L;
+    Criteria criteria = mock(Criteria.class);
+    when(hibernate.createCriteria(Goal.class)).thenReturn(criteria);
+    when(criteria.add(any(SimpleExpression.class))).thenReturn(criteria);
+    when(criteria.list()).thenReturn(Arrays.asList(new Goal("some goal", 1000), new Goal("second goal", 2000)));
+
+    assertRender(controller.post());
+
+    assertEquals(1,controller.errorsList.size());
+    assertTrue(controller.errorsList.contains("Tekkis viga."));
+  }
+}
