31941283d497bac7f8320580f03101a7b71dd8fe
xp <xp>
2014-10-30 09:48:55 +0200

    writes pid to file on startup

--- src/framework/Launcher.java
+++ src/framework/Launcher.java
@@ -7,7 +7,10 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.session.SessionHandler;
 
+import java.lang.management.ManagementFactory;
 import java.net.BindException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Launcher {
 
@@ -16,6 +19,10 @@ public class Launcher {
   public static void main(String... args) throws Exception {
     int port = 8080;
     if (args.length == 1) port = Integer.parseInt(args[0]);
+
+    String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+    Files.write(Paths.get("server.pid"), pid.getBytes());
+
     prepareLogging();
     Server server = createServer(port);
     try {
